eb515dd26a31b8c44d95faedaad69cc3f2e158ab
Jetaa <jeta.podrimcaku@student.uni-pr.edu>
2026-05-30 20:12:24 +0200

    Add module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,1 +1,17 @@
+module com.example.basketballsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
 
+    opens com.example.basketballsystem to javafx.fxml;
+    opens com.example.basketballsystem.controller to javafx.fxml;
+    opens com.example.basketballsystem.model to javafx.base;
+    opens application to javafx.graphics, javafx.fxml;
+
+    exports com.example.basketballsystem;
+    exports com.example.basketballsystem.model;
+    exports com.example.basketballsystem.controller;
+    exports com.example.basketballsystem.database;
+    exports com.example.basketballsystem.service;
+    exports application;
+}
