6ee64b74a79056c50f9bf0d0f2cb9c3820e95a25
Jetaa <jeta.podrimcaku@student.uni-pr.edu>
2026-05-30 20:25:14 +0200

    Add Launcher class

--- src/com/example/basketballsystem/Launcher.java
+++ src/com/example/basketballsystem/Launcher.java
@@ -1,1 +1,11 @@
+package com.example.basketballsystem;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(application.Main.class, args);
+    }
+}
+
 
