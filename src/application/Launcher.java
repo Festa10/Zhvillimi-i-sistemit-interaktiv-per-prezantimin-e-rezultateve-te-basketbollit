1e072b0bf0878c3999092b08246c1da963be72ca
Jetaa <jeta.podrimcaku@student.uni-pr.edu>
2026-05-30 20:20:09 +0200

    Add Launcher class

--- src/application/Launcher.java
+++ src/application/Launcher.java
@@ -1,1 +1,10 @@
+package application;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
 
